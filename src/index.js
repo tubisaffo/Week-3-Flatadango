577943f9a2a26341d0ac44ed929b47a0fc7b248f
tubisaffo <tubisaffo@gmail.com>
2024-04-09 09:22:44 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-let filmEndpoint = "http://localhost:3000/films"
+const endPoint = "http://localhost:3000/films"
 
 document.addEventListener("DOMContentLoaded", () => {
-getMovies();
-document.querySelector("#buy-ticket").addEventListener("click", handleBuyTicket);
+fetchMovies();
+// document.querySelector("#buy-ticket").addEventListener("click", handleBuyTicket);
 });
 
-function getMovies() {
-fetch(filmEndpoint)
+function fetchMovies() {
+fetch(endPoint)
 .then(res => res.json())
 .then(movies => {
 movies.forEach(movie => {renderMovieList(movie)})
+
+
 const firstMovie = document.querySelector("#id1");
 firstMovie.dispatchEvent(new Event("click"));
 })
 }
 
-console.log(getMovies);
 
 function renderMovieList(movie) {
 const li = document.createElement("li");
@@ -25,9 +26,10 @@ const ul = document.querySelector("#films");
 ul.appendChild(li);
 li.classList.add("film");
 li.classList.add('item');
-li.addEventListener("click", () => {handleMovieClick(movie)})
+li.addEventListener("click", () => {handleMovieClick(movie)
+})
 }
-console.log(renderMovieList);
+// console.log(renderMovieList);
 
 function handleMovieClick(movie) {
 const poster = document.querySelector("img#poster")
@@ -35,23 +37,45 @@ poster.src = movie.poster;
 poster.alt = movie.title;
 const info = document.querySelector("#showing");
 info.querySelector("#title").textContent = movie.title;
-info.querySelector("#runtime").textContent = movie.runtime+" minutes";
+info.querySelector("#runtime").textContent = `${movie.runtime} minutes`;
 info.querySelector("#film-info").textContent = movie.description;
 info.querySelector("#showtime").textContent = movie.showtime;
-info.querySelector("#ticket-num").textContent = movie.capacity - movie.tickets_sold + " remaining tickets";
+info.querySelector("#ticket-num").textContent = `${movie.capacity - movie.tickets_sold } remaining tickets`;
 }
-console.log(handleMovieClick);
+// console.log(handleMovieClick)
+document.querySelector("#buy-ticket").addEventListener("click", () => {
+handleBuyTicket(movie);
+});
+
 
 function handleBuyTicket(Ticket) {
 const ticketDiv = document.querySelector("#ticket-num");
 const tickets = ticketDiv.textContent.split(" ")[0];
 if (tickets > 0) {
-ticketDiv.textContent = tickets - 1 + " remaining tickets";
-}
-else if (tickets == 0) {
+    
+    fetch(endPoint + "/" + Ticket.id, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            tickets_sold: Ticket.tickets_sold + 1
+        })
+        
+    })
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data);
+        ticketDiv.textContent = tickets - 1 + " remaining tickets";
+    })
+.catch((err) => 
+        console.log(err));
+// ticketDiv.textContent = tickets - 1 + " remaining tickets";
+} else if (tickets == 0) {
 alert("No more tickets!");
 e.target.classList.add("sold-out");
 e.target.classList.remove("orange");
 }
+
 }
-console.log(handleBuyTicket);
+// console.log(handleBuyTicket);
